8e6df5463fb42be2927f9a64c6581b17c99e4b6e
nguyenhung291203 <nguyenhung291203@gmail.com>
2024-03-27 08:48:34 +0700

    08:48 27/03

---
+++ src/component/index.js
@@ -0,0 +1,12 @@
+import CardUserInfoCommon from "./card-user-info-common";
+import TotalPriceCommon from './total-price-common';
+import CardProductCommon from './card-product-common';
+import EmptyDataCommon from './empty-data-common';
+import FlatListOrderCommon from './flat-list-orders-common';
+import HeaderBackCommon from './header-back-common';
+import HeaderSearchCommon from './header-search-common';
+import LoadingCommon from './loading-common';
+import ToastNotificationCommon from './toast-notification-common';
+
+
+export default {CardUserInfoCommon,TotalPriceCommon,CardProductCommon, EmptyDataCommon, FlatListOrderCommon, HeaderBackCommon,  HeaderSearchCommon, LoadingCommon, ToastNotificationCommon}
