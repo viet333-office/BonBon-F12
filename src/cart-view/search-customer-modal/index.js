c0eac8c9cde91fa455ab5b90987d41a937910acc
MinhHieu012 <0967710509hieu@gmail.com>
2024-03-28 08:53:04 +0700

    Update: utils>constant.js VOLATILYTY to VOLATILITY

---
+++ src/cart-view/search-customer-modal/index.js
@@ -0,0 +1,10 @@
+import { AntDesign, EvilIcons } from "@expo/vector-icons"
+import { FlatList, Input, InputField, InputSlot, Modal, ModalBackdrop, ModalBody, ModalContent, ModalHeader, Pressable } from "@gluestack-ui/themed"
+import { CardUserInfoCommon, EmptyDataCommon, ToastNotificationCommon } from "../../component"
+import { CreateCustomerModal } from "../create-customer-modal"
+
+export const SearchCustomerModal = (props) => {
+    return (
+        
+    )
+}
