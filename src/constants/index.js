752e40f083f79d4a0ff6981f201313cfca2964c6
nguyenhung291203 <nguyenhung291203@gmail.com>
2024-03-28 15:17:18 +0700

    15:17 28/03

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 import * as importWareHouse from "./importWareHouseTypes";
 import * as listOrderType from "./listOrderType";
 import * as wareHouseTypes from "./wareHouseType";
-import * as cartTypes from './cartType'
+import * as cartTypes from './cartType';
+import * as listProductTypes from "./listProductType";
 
-export default { listOrderType, importWareHouse, wareHouseTypes, cartTypes };
+export default { listOrderType, importWareHouse, wareHouseTypes, cartTypes, listProductTypes};
