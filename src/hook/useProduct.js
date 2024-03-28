3847c1247b77d376782972d15fa3ef8cec1f5398
nguyenhung291203 <nguyenhung291203@gmail.com>
2024-03-28 11:22:03 +0700

    11:21 28/03

---
+++ src/hook/useProduct.js
@@ -0,0 +1,29 @@
+import {useDispatch, useSelector } from "react-redux";
+import { listProductAction, cartAction } from '../actions'
+export const useProduct=()=>{
+    const dispatch = useDispatch();
+    const listProductData = useSelector((state) => state.listProductData);
+
+    const notificationData = useSelector((state)=> state.isNotification);
+
+    const isFetching = useSelector((state) => state.isFetching );
+
+    const dispatchGetListProduct =(payload) =>{
+        dispatch(listProductRequest(payload))
+    }
+    const dispatchClearNotification = (payload)=>{
+        dispatch(ClearNotificationListProduct(payload))
+    }
+    const dispatchCreateItemProduct = (payload)=>{
+        dispatch(cartAction.createItemProductRequest(payload))
+    }
+    return {
+        notificationData,
+        listProductData,
+        listProductSearchData,
+        isFetching,
+        dispatchClearNotification,
+        dispatchGetListProduct,
+        dispatchCreateItemProduct
+    }
+}
