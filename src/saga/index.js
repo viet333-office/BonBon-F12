b2287cf8e03219f1275f650d2650cbd6682e7154
MinhHieu012 <0967710509hieu@gmail.com>
2024-03-28 16:02:30 +0700

    Done: Hieu_dev_NNN

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,7 +1,21 @@
 import wareHouseSaga from "./ware-house-saga"
+import { all } from "redux-saga/effects"
+import importWareHouseSaga from "./import-ware-house-saga"
+import wareHouseSaga from "./ware-house-saga"
+import cartSaga from "./cart-saga"
+import listOrderSaga from "./list-order-saga"
+import listProductSaga from "./list-product-saga"
+import customerSaga from "./customer-saga"
+import listImageProductSaga from "./list-image-product-saga"
 
-function *rootSaga() {
+function* rootSaga() {
     yield all([
-        ...wareHouseSaga
+        ...importWareHouseSaga,
+        ...wareHouseSaga,
+        ...cartSaga,
+        ...listOrderSaga,
+        ...listProductSaga,
+        ...customerSaga,
+        ...listImageProductSaga
     ])
 }
