29a0a8c5864d06ffe24de695575b4dcf118b07b1
manh2309 <manh230920011@gmail.com>
2024-03-29 09:30:24 +0700

    manh code

--- src/warehouse-view/index.js
+++ src/warehouse-view/index.js
@@ -7,12 +7,24 @@ import { useEffect, useMemo, useState } from "react";
 import useIsFocused from "@react-navigation/native";
 import useWareHouse from "../hook/useWareHouse";
 import { timeout, timeoutGet } from "../utils";
+import UpdatePriceProductModal from "./update-price-product-modal";
 
 const WarehouseScreen = (props) => {
   const isFocused = useIsFocused();
   const [isLoading, setLoading] = useState(false);
-  const { listWareHouseData, dispatchGetListWareHouse } = useWareHouse();
-
+  const {
+    listWareHouseData,
+    listWareHouseSearchData,
+    textSearch,
+    notificationData,
+    dispatchSearchListWareHouse,
+    dispatchGetListWareHouse,
+    dispatchClearNotificationWareHouse,
+    dispatchUpdateProductPrice,
+  } = useWareHouse();
+  const [isUpdatePriceModal, setIsUpdatePriceModal] = useState(false);
+  const [productSelected, setProductSelected] = useState({});
+  const [isNotification, setIsNotification] = useState(notificationData);
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
@@ -24,6 +36,7 @@ const WarehouseScreen = (props) => {
     <>
       <HeaderSearchCommon {...props} />
       <CardProductCommon data={listWareHouseData} {...props} />
+      <EmptyDataCommon />
       <LoadingCommon isOpen={isLoading} />
     </>
   );
--- src/warehouse-view/update-price-product-modal/index.js
+++ src/warehouse-view/update-price-product-modal/index.js
@@ -22,6 +22,7 @@ import {
   textConst,
   timeoutGet,
 } from "../../utils";
+import { Keyboard } from "react-native";
 export const UpdatePriceProductModal = (props) => {
   const { isOpen, closeModal, updatePrice, data } = props;
   const [isValidateInput, setIsValidateInput] = useState(false);
@@ -45,5 +46,97 @@ export const UpdatePriceProductModal = (props) => {
       // Dùng để thay đổi giá trị thành kiểu dữ liệu string
     }
   }, [data, isOpen]);
-  const onPressCancel = () => {};
+  const onPressCancel = () => {
+    closeModal();
+  };
+  const onPressUpdate = () => {
+    if (!isValidateInput) {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        updatePrice(
+          {
+            codeProduct: data.codeProduct,
+            floorPrice: parseInt(floorPrice),
+          },
+          timeoutGet
+        );
+      });
+    } else {
+      Keyboard.dismiss();
+    }
+  };
+  return (
+    <Modal
+      style={styles.container}
+      avoidKeyboard={true}
+      isKeyboardDismissable={true}
+      isOpen={isOpen}
+    >
+      {" "}
+      UGSPK-HTML - 1
+      <ModalBackdrop onPress={onPressCancel} /> UGSPK-HTML - 2
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss()}>
+        {" "}
+        UGSPK-HTML - 3
+        <ModalContent style={styles.content}>
+          <HStack style={styles.body}>
+            <Image
+              source={{ uri: data.avatar }}
+              alt=""
+              style={styles.image}
+              flex={1}
+            />
+            <VStack
+              space="xs"
+              style={styles.contentRight}
+              alignItems="center"
+              flex={1}
+            >
+              <Text bold size="lg" numberOfLines={1}>
+                {data.name}
+              </Text>
+              <Text opacity={0.5}>:{textConst.FLOOR_PRICE}</Text>
+              <Text bold color={color.plumRed}>
+                {formatMoney(data.floorPrice)}
+              </Text>
+              <Text opacity={0.5}>:{textConst.NEW_FLOOR_PRICE}</Text>
+              <Input
+                variant="outline"
+                style={styles.input}
+                isInvalid={isValidateInput}
+              >
+                <InputField
+                  keyboardType="number-pad"
+                  placeholder=""
+                  style={styles.inputField}
+                  onChangeText={onChangeTextInput}
+                  value={formatMoney(floorPrice)}
+                />
+              </Input>
+              <HStack space="sm" style={styles.buttonGroup}>
+                <Button
+                  size="sm"
+                  style={styles.buttonRadius}
+                  bgColor={color.plumRed}
+                  onPress={onPressCancel}
+                >
+                  <ButtonText>{buttonText.BUTTON_CANCEL}</ButtonText>
+                </Button>
+                <Button
+                  size="sm"
+                  style={styles.buttonRadius}
+                  bgColor={color.darkGreen}
+                  onPress={onPressUpdate}
+                >
+                  <ButtonText>{buttonText.BUTTON_UPDATE}</ButtonText>
+                </Button>
+              </HStack>
+            </VStack>
+          </HStack>
+          <LoadingCommon isLoading={isLoading} />
+        </ModalContent>
+      </TouchableWithoutFeedback>
+    </Modal>
+  );
 };
