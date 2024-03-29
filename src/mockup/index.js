1b9b579ea29f3e72bd98f27bbe6744f6cbbd2fa5
MinhHieu012 <0967710509hieu@gmail.com>
2024-03-29 09:02:26 +0700

    Done: Fix bugs thiếu thư viện > navigation > index.js

--- src/mockup/index.js
+++ src/mockup/index.js
@@ -1,9 +1,9 @@
-import { adminCartData } from "./admin-cart"; // "./AdminCart" -> "./admin-cart"
-import { listCustomerData } from "./list-customer"; // "./ListCustomer" -> "./list-customer"
-import { listImageProductData } from "./list-product-image"; // "./ListProductImage" -> "./list-product-image"
-import { listOrderData } from "./list-order"; // "./ListOrder" -> "./list-order"
-import { listProduct } from "./list-product"; // "./ListProduct" -> "./list-product"
-import { saleCartData } from "./sale-cart"; // "./SaleCart" -> "./sale-cart"
+import { adminCartData } from "./admin-cart";
+import { listCustomerData } from "./list-customer";
+import { listImageProductData } from "./list-product-image";
+import { listOrderData } from "./list-order";
+import { listProduct } from "./list-product";
+import { saleCartData } from "./sale-cart";
 
 export default {
     adminCartData,listCustomerData,listImageProductData,listOrderData,listProduct,saleCartData
